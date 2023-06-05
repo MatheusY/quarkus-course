fc81c92c214081a05168961b9b433c3deca440a1
Matheus <matheus.matsubara12@gmail.com>
2023-06-05 20:07:56 -0300

    Init project

---
+++ customer/src/main/test/java/org/mmatsubara/quarkus/jdbc/CustomerRepository.java
@@ -0,0 +1,8 @@
+package org.mmatsubara.quarkus.jdbc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
